ef93d319f12ad99452ccdc55bc0a7ca13c022493
Кузьмин Юрий <kuzminyv@m2.ru>
2022-01-25 05:42:31 +0300

    update homework

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,14 +1,12 @@
 resource "yandex_lb_target_group" "wp_tg" {
   name      = "wp-target-group"
 
-  target {
-    subnet_id = yandex_vpc_subnet.wp-subnet-a.id
-    address   = yandex_compute_instance.wp-app-1.network_interface.0.ip_address
-  }
-
-  target {
-    subnet_id = yandex_vpc_subnet.wp-subnet-b.id
-    address   = yandex_compute_instance.wp-app-2.network_interface.0.ip_address
+  dynamic target {
+    for_each = local.wp-app-vm-list
+    content {
+      subnet_id = target.value.network_interface[0].subnet_id
+      address = target.value.network_interface[0].ip_address
+    }
   }
 }
 
--- terraform/wp-app.tf
+++ terraform/wp-app.tf
@@ -1,5 +1,12 @@
-resource "yandex_compute_instance" "wp-app-1" {
-  name = "wp-app-1"
+locals {
+  wp-app-vm-list = tolist(concat(yandex_compute_instance.wp-app-a[*],
+    yandex_compute_instance.wp-app-b[*],
+    yandex_compute_instance.wp-app-c[*]))
+}
+
+resource "yandex_compute_instance" "wp-app-a" {
+  count = var.count_compute_instances
+  name = "wp-app-a-${count.index}"
   zone = "ru-central1-a"
 
   resources {
@@ -14,7 +21,6 @@ resource "yandex_compute_instance" "wp-app-1" {
   }
 
   network_interface {
-    # Указан id подсети default-ru-central1-a
     subnet_id = yandex_vpc_subnet.wp-subnet-a.id
     nat       = true
   }
@@ -24,8 +30,9 @@ resource "yandex_compute_instance" "wp-app-1" {
   }
 }
 
-resource "yandex_compute_instance" "wp-app-2" {
-  name = "wp-app-2"
+resource "yandex_compute_instance" "wp-app-b" {
+  count = var.count_compute_instances
+  name = "wp-app-b-${count.index}"
   zone = "ru-central1-b"
 
   resources {
@@ -40,8 +47,33 @@ resource "yandex_compute_instance" "wp-app-2" {
   }
 
   network_interface {
-    # Указан id подсети default-ru-central1-b
     subnet_id = yandex_vpc_subnet.wp-subnet-b.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/yc.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "wp-app-c" {
+  count = var.count_compute_instances
+  name = "wp-app-c-${count.index}"
+  zone = "ru-central1-c"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80viupr3qjr5g6g9du"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.wp-subnet-c.id
     nat       = true
   }
 
